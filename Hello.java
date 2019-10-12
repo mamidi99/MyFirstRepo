2f2a93d78fed6f7a3a8f365e208b28b6cb2ab3cc
Lingareddy Mamidi <mamidi99@yahoo.com>
2019-10-12 23:54:12 +0000

    Added comment for Welcome Git print

--- Hello.java
+++ Hello.java
@@ -3,7 +3,8 @@ c class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-
+	
+	//Prints "Welcome to GIT".
 	System.out.println("Welocme to GIT");
 
     }
